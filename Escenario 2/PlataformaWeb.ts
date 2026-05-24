0a9d526b7d55735bc212a4201cd053dc75323718
Sergio López <64379814+Sergio-La@users.noreply.github.com>
2026-05-24 12:19:46 -0500

    Delete Escenario 2/PlataformaWeb.ts

--- Escenario 2/PlataformaWeb.ts
+++
@@ -1,8 +0,0 @@
-import {IPlataforma} from "./IPlataforma";
-
-export class PlataformaWeb implements IPlataforma {
-    enviarNotificacion(mensaje: string): void {
-        console.log(`Enviando notificación web: ${mensaje}`);
-    }
-
-}
