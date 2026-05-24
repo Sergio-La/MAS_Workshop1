7172d9186673354f55681101085d5704e0915815
Sergio López <64379814+Sergio-La@users.noreply.github.com>
2026-05-24 12:19:38 -0500

    Delete Escenario 2/PlataformaMovil.ts

--- Escenario 2/PlataformaMovil.ts
+++
@@ -1,8 +0,0 @@
-import {IPlataforma} from "./IPlataforma";
-
-export class PlataformaMovil implements IPlataforma {
-    enviarNotificacion(mensaje: string): void {
-        console.log(`Enviando notificación móvil: ${mensaje}`);
-    }
-
-}   
