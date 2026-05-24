6d657b044d1d2ffb62ef3e3872ca26b74ecea3a8
Sergio López <64379814+Sergio-La@users.noreply.github.com>
2026-05-24 12:19:13 -0500

    Delete Escenario 2/NotificacionAlerta.ts

--- Escenario 2/NotificacionAlerta.ts
+++
@@ -1,9 +0,0 @@
-import { IPlataforma } from "./IPlataforma";
-import { Notificacion } from "./Notificacion";
-
-export class NotificacionAlerta extends Notificacion {
-    enviar(): void {
-        const mensaje = "¡Alerta! Se ha detectado una situación crítica.";
-        this.plataforma.enviarNotificacion(mensaje);
-    }
-}
