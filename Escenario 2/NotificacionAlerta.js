0e3395941f0aa4d447ca3df8fec079c117f7ac84
Sergio López <64379814+Sergio-La@users.noreply.github.com>
2026-05-24 12:52:34 -0500

    Delete Escenario 2/NotificacionAlerta.js

--- Escenario 2/NotificacionAlerta.js
+++
@@ -1,7 +0,0 @@
-import { Notificacion } from "./Notificacion";
-export class NotificacionAlerta extends Notificacion {
-    enviar() {
-        const mensaje = "¡Alerta! Se ha detectado una situación crítica.";
-        this.plataforma.enviarNotificacion(mensaje);
-    }
-}
