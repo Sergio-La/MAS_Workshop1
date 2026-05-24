fa5a2964905daf52ff13d527c45a696ca1c766cf
Sergio López <64379814+Sergio-La@users.noreply.github.com>
2026-05-24 12:19:30 -0500

    Delete Escenario 2/PlataformaEscritorio.ts

--- Escenario 2/PlataformaEscritorio.ts
+++
@@ -1,7 +0,0 @@
-import {IPlataforma} from "./IPlataforma";
-
-export class PlataformaEscritorio implements IPlataforma {
-    enviarNotificacion(mensaje: string): void {
-        console.log(`Enviando notificación de escritorio: ${mensaje}`);
-    }
-}
