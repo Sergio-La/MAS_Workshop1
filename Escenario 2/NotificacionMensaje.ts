6fc2987e544765170d43742b2541e0cad4b3dcf1
Sergio López <64379814+Sergio-La@users.noreply.github.com>
2026-05-24 12:19:22 -0500

    Delete Escenario 2/NotificacionMensaje.ts

--- Escenario 2/NotificacionMensaje.ts
+++
@@ -1,9 +0,0 @@
-import { Notificacion } from "./Notificacion";
-
-
-export class NotificacionMensaje extends Notificacion {
-    enviar(): void {
-        const mensaje = "¡Nuevo mensaje! Tienes un nuevo mensaje en tu bandeja de entrada.";
-        this.plataforma.enviarNotificacion(mensaje);
-    }
-}
